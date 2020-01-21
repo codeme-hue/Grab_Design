fa1b90519c8e98e0ecb6adc066ee9c68069ef1ef
kardihaekal <kardyhaekal@gmail.com>
2020-01-21 15:01:53 +0800

    kotlin is fun

---
+++ app/src/main/java/id/kardihaekal/grab_design/MainActivity.kt
@@ -0,0 +1,47 @@
+package id.kardihaekal.grab_design
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        greeting()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun greeting() {
+
+        val calender:Calendar = Calendar.getInstance()
+        val timeOfDay:Int = calender.get(Calendar.HOUR_OF_DAY)
+        val greetImg = findViewById<ImageView>(R.id.greeting_img)
+        val greetText = findViewById<TextView>(R.id.greeting_text)
+
+        /**
+         * the key is here
+         * */
+        if (timeOfDay >= 0 && timeOfDay < 12){
+            greetText.setText("Selamat Pagi\nAzhar")
+            greetImg.setImageResource(R.drawable.img_default_half_morning)
+        } else if (timeOfDay >= 12 && timeOfDay < 15) {
+            greetText.setText("Selamat Siang\nAzhar")
+            greetImg.setImageResource(R.drawable.img_default_half_afternoon)
+        } else if (timeOfDay >= 15 && timeOfDay < 18) {
+            greetText.setText("Selamat Sore\nAzhar")
+            greetImg.setImageResource(R.drawable.img_default_half_without_sun)
+        } else if (timeOfDay >= 18 && timeOfDay < 24) {
+            greetText.setText("Selamat Malam\nAzhar")
+            greetText.setTextColor(Color.WHITE)
+            greetImg.setImageResource(R.drawable.img_default_half_night)
+        }
+    }
+
+}
